8c175dcaab091c447e5ec1169bdab60a2559337b
samuelsantosr360 <samuelsantosr360@gmail.com>
2026-03-14 10:29:33 -0300

    alter class

--- app/models/cidade.php
+++ app/models/cidade.php
@@ -1,10 +1,44 @@
-<?php 
+<?php
 
-class Cidade{
+class Cidade
+{
     private $id;
     private $nome;
     private $estado;
-    
-}
 
+    public function __construct($nome, $estado = null)
+    {
+        $this->setNome($nome);
+        $this->setEstado($estado);
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function getNome()
+    {
+        return $this->nome;
+    }
+    public function getEstado()
+    {
+        return $this->estado;
+    }
+
+    public function setId($id)
+    {
+        $this->id = (int) $id;
+    }
+    public function setNome($nome)
+    {
+
+        if (!empty($nome)) {
+            $this->nome = $nome;
+        }
+
+    }
+    public function setEstado($estado){
+        $this->estado = $estado;
+    }
+}
 ?>
