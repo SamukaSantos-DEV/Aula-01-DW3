a97004b5161a229b79e0f205c77bbb8d55c8dcb2
samuelsantosr360 <samuelsantosr360@gmail.com>
2026-03-28 10:26:53 -0300

    conection

---
+++ config/database.php
@@ -0,0 +1,19 @@
+<?php
+$host = "localhost";
+$dbname = "imobiliariajg";
+$username = "root";
+$password = "";
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8",
+        $username,
+        $password,
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Conectado com sucesso!";
+} catch (PDOException $e) {
+    echo "Erro na conexão: " . $e->getMessage();
+}
+?>
