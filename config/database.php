5742e3656e1ee86fa8ea6f3559a2794c35a8a959
samuelsantosr360 <samuelsantosr360@gmail.com>
2026-03-28 10:42:04 -0300

    alte class private

--- config/database.php
+++ config/database.php
@@ -1,19 +1,29 @@
 <?php
-$host = "localhost";
-$dbname = "imobiliariajg";
-$username = "root";
-$password = "";
 
-try {
-    $pdo = new PDO(
-        "mysql:host=$host;dbname=$dbname;charset=utf8",
-        $username,
-        $password,
-    );
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+class Database
+{
+    private static $instance = null;
+    private $connection;
+    private function __construct()
+    {
+        $host = "localhost";
+        $dbname = "imobiliariajg";
+        $username = "root";
+        $password = "";
 
-    echo "Conectado com sucesso!";
-} catch (PDOException $e) {
-    echo "Erro na conexão: " . $e->getMessage();
+        try {
+            $pdo = new PDO(
+                "mysql:host=$host;dbname=$dbname;charset=utf8",
+                $username,
+                $password,
+            );
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            echo "Conectado com sucesso!";
+        } catch (PDOException $e) {
+            die("Erro na conexão: " . $e->getMessage());
+        }
+    }
 }
+
 ?>
